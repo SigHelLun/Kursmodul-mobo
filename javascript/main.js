b3699453924dc8055df2ee16fa87c36210509229
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-11-07 08:36:01 +0100

    update
    sheeet

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+function quiztest()
+{
+//question 1
+let svar1 = '4';
+let riktig = 0;
+const getInput1 = document.getElementById('input1').value;
+const feil = document.getElementById('answer1').textContent = "svaret er feil";
+if(getInput1 == svar1){ 
+    document.getElementById('answer1').textContent = "riktig svaret var 4";
+    riktig++;
+}
+else {
+    feil;
+};
+//question 2
+const Bu5 = document.getElementById('Bu5');
+
+
+
+
+
+
+
+
+
+
+}
+
