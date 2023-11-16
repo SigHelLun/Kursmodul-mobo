36ebacc4582c2caa5bcc37e8058f9344d49742bf
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-11-16 11:56:46 +0100

    update started on texdown buttons

--- javascript/course.js
+++ javascript/course.js
@@ -15,3 +15,44 @@ else {
 //question 2
 const Bu5 = document.getElementById('Bu5');
 }
+
+
+
+let CPUs = document.getElementById("CPU")
+let memory = document.getElementById("Memory-slot")
+/* let IO = document.getElementById("dropdown-btn")
+let CMOS = document.getElementById("dropdown-btn")
+let CMOSb = document.getElementById("dropdown-btn")
+let PCH = document.getElementById("dropdown-btn")
+let Temp = document.getElementById("dropdown-btn")
+let MBCPU = document.getElementById("dropdown-btn") */
+
+function droptext() {
+    let CPUdrop = document.getElementById("CPUdrop");
+    let Memorydrop = document.getElementById("Memorydrop")
+    if (CPUdrop.style.display === "block") {
+      CPUdrop.style.display = "none";
+      CPUs.textContent = CPUs.textContent -"▲";
+      CPUs.textContent = CPUs.textContent +"▼";
+    }
+    else {
+        CPUdrop.style.display = "block";
+        CPUs.textContent = CPUs.textContent - "▼" 
+        CPUs.textContent = CPUs.textContent + "▲" 
+  
+      }
+    if (Memorydrop.style.display === "block") {
+        Memorydrop.style.display = "none";
+        memory.textContent = memory.textContent + CPUs.memory -"▲";
+        memory.textContent = memory.textContent + CPUs.memory +"▼";
+    }
+    else{
+        
+        CPUdrop.style.display = "block";
+        CPUs.textContent = CPUs.textContent - "▼" 
+        CPUs.textContent = CPUs.textContent + "▲" 
+    }
+    } 
+    
+
+  
