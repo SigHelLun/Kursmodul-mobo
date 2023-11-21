ee94b839caa7dd6645ecb8cbe787d88c514d02bf
Sigurd <116567255+SigHelLun@users.noreply.github.com>
2023-11-21 21:59:49 +0100

    started on quiz v.0.1 beta
    bitch ass mf who reading this shit bruh

--- javascript/course.js
+++ javascript/course.js
@@ -1,34 +1,51 @@
-function quiztest()
-{
-//question 1
-let svar1 = '4';
-let riktig = 0;
-const getInput1 = document.getElementById('input1').value;
-const feil = document.getElementById('answer1').textContent = "svaret er feil";
-if(getInput1 == svar1){ 
-    document.getElementById('answer1').textContent = "riktig svaret var 4";
-    riktig++;
-}
-else {
-    feil;
-};
-//question 2
-const Bu5 = document.getElementById('Bu5');
-}
-
-
-//
-function droptext(elementId) {
-    let dropElement = document.getElementById(elementId + "drop");
-    let buttonElement = document.getElementById(elementId);
-
-    if (dropElement.style.display === "block") {
-        dropElement.style.display = "none";
-        buttonElement.textContent = buttonElement.textContent.replace("▲", "▼");
-    } else {
-        dropElement.style.display = "block";
-        buttonElement.textContent = buttonElement.textContent.replace("▼", "▲");
+const myQuiz = [
+    {
+      question: "Kva er eit Hovudkort?",
+      answers: {
+        a: 'Ein PCB',
+        b: 'Ein man på ski',
+        c: 'Ein type MOBO',
+        d: 'Ein variant av PCI-e 4.0'
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "Kva heite AMD og INTEL sinn CPU socket standard?",
+      answers: {
+        a: 'AM4 & zen3',
+        b: 'Intel og AMD CPU',
+        c: 'AM LGA',
+        d: 'LGA 1200'
+      },
+      correctAnswer: 'c'
     }
-}
-
+  ];
+  const hiding = document.getElementById("hideQ")
+  const question = document.getElementById("Q");
+  const a = document.getElementById("a");
+  const b = document.getElementById("b");
+  const c = document.getElementById("c");
+  const d = document.getElementById("d");
+  
+  function start() {
+    if (question.innerHTML === "") {
+      hiding.style.display = "block"
+      question.innerHTML = myQuiz[0].question;
+      a.innerHTML = myQuiz[0].answers.a;
+      b.innerHTML = myQuiz[0].answers.b;
+      c.innerHTML = myQuiz[0].answers.c;
+      d.innerHTML = myQuiz[0].answers.d;
+    }
+  }
+  
+  //dropdown box code
+  function droptext(downID) {
+    let dropElement = document.getElementById(downID + "drop");
+  
+    if (dropElement.style.display === "block") {
+      dropElement.style.display = "none";
+    } else {
+      dropElement.style.display = "block";
+    }
+  }
   
